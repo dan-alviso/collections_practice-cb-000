7c3a8cb25ed263f701bbdb16d87b6184b5f484d9
Daniel Alviso <dan.alviso2@yahoo.com>
2017-06-16 12:39:25 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,65 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      -1
+    elsif a > b
+      1
+    end
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  x = array[1]
+  array[1] = array[2]
+  array[2] = x
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(strings)
+  strings.each { |string| string[2] = "$"}
+  strings
+end
+
+def find_a(strings)
+  strings.select { |string| string.start_with?("a")}
+end
+
+def sum_array(numbers)
+  total = 0
+  numbers.each { |number| total += number}
+  total
+end
+
+def add_s(words)
+  words.each_with_index { |word, index| word << "s" if index != 1}
+end
